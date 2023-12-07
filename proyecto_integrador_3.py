42382beea8f272e861bc912fcb758c6b19fb141e
PreposterousName <88461623+PreposterousName@users.noreply.github.com>
2023-12-06 22:27:29 -0600

    Update proyecto_integrador_3.py

--- proyecto_integrador_3.py
+++ proyecto_integrador_3.py
@@ -1,12 +1,13 @@
 import os
 from readchar import readkey, key
 
+def borrar_imprimir_terminal(num):
+    os.system('cls' if os.name=='nt' else 'clear')
+    print(num)
+    
 n = 0
 while n < 50:
-        k = readkey()
-        if k == "n":
-            n += 1
-            os.system('cls' if os.name=='nt' else 'clear')
-            print(n)
-
-            
+    k = readkey()
+    if k == "n":
+        n += 1
+        borrar_imprimir_terminal(n)
